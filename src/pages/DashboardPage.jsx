4ee1a36a6323c77009961a6d548f3138dd9a468b
rkritzar39 <rkritzar39@gmail.com>
2026-05-01 11:24:35 -0400

    Implement DashboardPage to display course list

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,1 +1,29 @@
+import courses from "../data/courses.json";
+import { Link } from "react-router-dom";
 
+export default function DashboardPage() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+
+      <div className="grid">
+        {courses.map((course) => (
+          <Link
+            to={`/course/${course.id}`}
+            key={course.id}
+            className="card"
+          >
+            <h3>{course.name}</h3>
+            <p>{course.term}</p>
+
+            <div className="bar">
+              <div style={{ width: course.progress + "%" }} />
+            </div>
+
+            <small>{course.progress}% complete</small>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
