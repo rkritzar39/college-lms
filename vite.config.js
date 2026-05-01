c7eb623f31c2c31d34c15e8fe5fffd843b49a578
rkritzar39 <rkritzar39@gmail.com>
2026-05-01 12:22:26 -0400

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/college-lms/" // CHANGE THIS to your repo name
-});
