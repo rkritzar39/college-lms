f162eefebd776f4cda605dc0eb98bdb62f31f323
rkritzar39 <rkritzar39@gmail.com>
2026-05-01 11:32:36 -0400

    Refactor router to use component imports directly

--- src/app/router.jsx
+++ src/app/router.jsx
@@ -1,17 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Layout from "../components/layout/Layout";
-
-import DashboardPage from "../pages/DashboardPage";
-import CoursePage from "../pages/CoursePage";
-import GradebookPage from "../pages/GradebookPage";
+import Dashboard from "../components/dashboard/Dashboard";
+import CoursePage from "../components/course/CoursePage";
+import GradebookPage from "../components/gradebook/GradebookPage";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Layout>
         <Routes>
-          <Route path="/" element={<DashboardPage />} />
+          <Route path="/" element={<Dashboard />} />
           <Route path="/course/:id" element={<CoursePage />} />
           <Route path="/grades" element={<GradebookPage />} />
         </Routes>
