7af4620fde6e94df7c48a10d1e2ba27ae59411bb
rkritzar39 <rkritzar39@gmail.com>
2026-05-01 11:50:48 -0400

    Refactor Layout component to use Outlet

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,14 +1,17 @@
+import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import Topbar from "./Topbar";
 
-export default function Layout({ children }) {
+export default function Layout() {
   return (
     <div className="app-shell">
       <Sidebar />
 
       <div className="main-shell">
         <Topbar />
-        <div className="page">{children}</div>
+        <div className="page">
+          <Outlet />
+        </div>
       </div>
     </div>
   );
