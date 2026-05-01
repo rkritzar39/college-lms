af6f8462ae02d48d668485cd957457198f3bfad4
rkritzar39 <rkritzar39@gmail.com>
2026-05-01 12:10:49 -0400

    Add GradebookPage component to display assignments

--- src/components/gradebook/Gradebook.jsx
+++ src/components/gradebook/Gradebook.jsx
@@ -1,1 +1,20 @@
+import assignments from "../../data/assignments.json";
+import courses from "../../data/courses.json";
 
+export default function GradebookPage() {
+  return (
+    <div>
+      <h1>Gradebook</h1>
+
+      {assignments.map((a, i) => {
+        const course = courses.find((c) => c.id === a.courseId);
+
+        return (
+          <div key={i}>
+            <strong>{course?.name}</strong> | {a.title} | {a.status} | {a.grade ?? "-"}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
