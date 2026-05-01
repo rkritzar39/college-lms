3c0887960cc7434b80602cb382f6aeb69301a96b
rkritzar39 <rkritzar39@gmail.com>
2026-05-01 11:25:43 -0400

    Implement routing with BrowserRouter and pages

--- src/app/router.jsx
+++ src/app/router.jsx
@@ -1,1 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Layout from "../components/layout/Layout";
+
+import DashboardPage from "../pages/DashboardPage";
+import CoursePage from "../pages/CoursePage";
+import GradebookPage from "../pages/GradebookPage";
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<DashboardPage />} />
+          <Route path="/course/:id" element={<CoursePage />} />
+          <Route path="/grades" element={<GradebookPage />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
+}
