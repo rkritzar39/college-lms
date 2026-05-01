588701f0f847b46ea0c38aaa8037a07f5a65bcb5
rkritzar39 <rkritzar39@gmail.com>
2026-05-01 11:20:36 -0400

    Add Topbar component for header layout

--- src/components/layout/Topbar.jsx
+++ src/components/layout/Topbar.jsx
@@ -1,1 +1,13 @@
+export default function Topbar() {
+  return (
+    <header className="topbar">
+      <div className="left">
+        <span>College LMS</span>
+      </div>
 
+      <div className="right">
+        <span>Progress Tracker</span>
+      </div>
+    </header>
+  );
+}
