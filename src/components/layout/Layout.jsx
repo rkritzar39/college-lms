90d6b1208c51c580e5cd0647ff3fbafb74e624da
rkritzar39 <rkritzar39@gmail.com>
2026-05-01 11:19:29 -0400

    Add Sidebar and Topbar components to Layout

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,1 +1,13 @@
-
+import Sidebar from "./Sidebar";
+import Topbar from "./Topbar";
+export default function Layout({ children }) {
+  return (
+    <div className="app-shell">
+      <Sidebar />
+      <div className="main-shell">
+        <Topbar />
+        <div className="page-content">{children}</div>
+      </div>
+    </div>
+  );
+}
