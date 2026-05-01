b3a2d4d6140faf1398a5a131d998124dcfb60708
rkritzar39 <rkritzar39@gmail.com>
2026-05-01 11:33:39 -0400

    Change page-content div to page div in Layout

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,12 +1,14 @@
 import Sidebar from "./Sidebar";
 import Topbar from "./Topbar";
+
 export default function Layout({ children }) {
   return (
     <div className="app-shell">
       <Sidebar />
+
       <div className="main-shell">
         <Topbar />
-        <div className="page-content">{children}</div>
+        <div className="page">{children}</div>
       </div>
     </div>
   );
