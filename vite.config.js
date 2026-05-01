e668dee14748f126ea91dc5c0735b77872f2a092
rkritzar39 <rkritzar39@gmail.com>
2026-05-01 12:05:48 -0400

    Update base path comment in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-
-  // IMPORTANT: change this to your repo name when deploying GitHub Pages
-  base: "/college-lms/"
+  base: "/college-lms/" // CHANGE THIS to your repo name
 });
