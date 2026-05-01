27e14685cf0ca01f6dae175519811b40345c3363
rkritzar39 <rkritzar39@gmail.com>
2026-05-01 11:59:05 -0400

    Change BrowserRouter to HashRouter in router

--- src/app/router.jsx
+++ src/app/router.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Layout from "../components/layout/Layout";
 import Dashboard from "../components/dashboard/Dashboard";
@@ -7,7 +7,7 @@ import GradebookPage from "../components/gradebook/GradebookPage";
 
 export default function Router() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Dashboard />} />
@@ -15,6 +15,6 @@ export default function Router() {
           <Route path="/grades" element={<GradebookPage />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
